c0c2d90fb9590da03a2beeb89cb8f94db94838c3
Max Miller <maxmiller413@gmail.com>
2020-11-28 21:33:21 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,7 @@
 first_number = 10 
 second_number = 2 
 sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
 # Add your variables here
